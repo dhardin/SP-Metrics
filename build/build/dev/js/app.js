50f328065e6d0dff01cf2474c6da154899198e8d
Dustin Hardin <dhardin07@gmail.com>
2018-05-04 13:36:35 -0400

    promises executing out of order in init

--- build/build/dev/js/app.js
+++ build/build/dev/js/app.js
@@ -355,46 +355,44 @@ var app = new Vue({
           });
         }
       }).then(function(result){
-      return new Promise(function(resolve, reject){
-          if(that.testing){
-            resolve();
-          } else {
-            that.getListFileds(function(data){
-              if(data.length > 0){
-                fieldMap = data.reduce(function(map, obj) {
-                    map[obj.Title] = obj;
-                    return map;
-                }, {});
-                _.assign(that.state_map.fieldMap, fieldMap);
-              }
-              resolve();
-            }, function(error){
-              that.toggleLoading({isloading: true, message: error.message, canCancel:false, canClose: true});
-            });
-          }
-        });
-      }).then(function(result){
         return new Promise(function(resolve, reject){
-          if(that.config.ID > 0 && !that.config.hasFilterDetection){
-            //we'll want to wait on filters to be generated from out filter component first if they're needed
-            //this way we avoid more web service calls.
-            that.getData(function(data){
-              that.populateMetrics(data);
+            if(that.testing){
               resolve();
-            }, function(error){
-              that.toggleLoading({isloading: true, message: error.message, canCancel:false, canClose: true});
-            });
-          } else if (that.config.hasFilterDetection){
-            that.delayedFetch = true;
-            resolve();
-          } else if(that.testing){
-            that.populateMetrics([]);
-            resolve();
-          } else {
-            resolve();
-          }
-        });
-      }).then(function(result){
+            } else {
+              that.getListFileds(function(data){
+                if(data.length > 0){
+                  fieldMap = data.reduce(function(map, obj) {
+                      map[obj.Title] = obj;
+                      return map;
+                  }, {});
+                  _.assign(that.state_map.fieldMap, fieldMap);
+                }
+                return;
+              }, function(error){
+                that.toggleLoading({isloading: true, message: error.message, canCancel:false, canClose: true});
+              });
+            }
+          }).then(function(result){
+            if(that.config.ID > 0 && !that.config.hasFilterDetection){
+              //we'll want to wait on filters to be generated from out filter component first if they're needed
+              //this way we avoid more web service calls.
+              that.getData(function(data){
+                that.populateMetrics(data);
+                resolve();
+              }, function(error){
+                that.toggleLoading({isloading: true, message: error.message, canCancel:false, canClose: true});
+              });
+            } else if (that.config.hasFilterDetection){
+              that.delayedFetch = true;
+              resolve();
+            } else if(that.testing){
+              that.populateMetrics([]);
+              resolve();
+            } else {
+              resolve();
+            }
+        })
+    }).then(function(result){
         if(Object.keys(that.metrics).length > 0 || that.editing){
           that.toggleLoading({isloading: false, message: '', canCancel:false, canClose: false});
         } else if(!that.delayedFetch){
