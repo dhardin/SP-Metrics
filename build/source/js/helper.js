c1224f469dd489063e4dec8dc609975cec515801
Dustin Hardin <dhardin07@gmail.com>
2018-05-04 14:46:27 -0400

    update to helper decoder

--- build/source/js/helper.js
+++ build/source/js/helper.js
@@ -2,7 +2,7 @@ var app_helper = {
   methods: {
       decodeSharePointFieldUri: function(fieldName){
         var decodedFieldName = decodeURIComponent(fieldName); //first initial decode from URI
-         return fieldName.replace(/%5f/g, '_');
+         return decodedFieldName.replace(/%5f/g, '_');
       }
     }
   };
