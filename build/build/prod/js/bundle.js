706739bbed77eacc6b0f08dfa18ca3c1c27c9f68
Dustin Hardin <dhardin07@gmail.com>
2018-05-04 15:42:38 -0400

    prod bundle

--- build/build/prod/js/bundle.js
+++ build/build/prod/js/bundle.js
@@ -174,7 +174,7 @@ var app_helper = {
   methods: {
       decodeSharePointFieldUri: function(fieldName){
         var decodedFieldName = decodeURIComponent(fieldName); //first initial decode from URI
-         return decodedFieldName.replace(/%5f/g, '_');
+         return decodedFieldName.replace(/%5F/g, '_');
       }
     }
   };
