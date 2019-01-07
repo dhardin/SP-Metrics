2be983716ef4942c82b01e54b8892f8bf1877746
Dustin Hardin <dhardin07@gmail.com>
2019-01-07 09:17:15 -0500

    rebuild w/out hashed filenames.

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,26 @@ module.exports = {
     },
     chainWebpack: (config) => {
         const svgRule = config.module.rule('svg');
-    
+
         svgRule.uses.clear();
-    
+
         svgRule
-          .use('vue-svg-symbol-loader')
-          .loader('vue-svg-symbol-loader')
-          .end()
-      },
+            .use('vue-svg-symbol-loader')
+            .loader('vue-svg-symbol-loader')
+            .end()
+
+        if (config.plugins.has('extract-css')) {
+            const extractCSSPlugin = config.plugin('extract-css')
+            extractCSSPlugin && extractCSSPlugin.tap(() => [{
+                filename: '[name].css',
+                chunkFilename: '[name].css'
+            }])
+        }
+    },
+    configureWebpack: {
+        output: {
+            filename: '[name].js',
+            chunkFilename: '[name].js'
+        }
+    }
 }
