aec3cc1465714cfecee0a14525480ecdf1072cd0
Dustin Hardin <dhardin07@gmail.com>
2019-01-29 11:36:10 -0500

    style update.

--- src/components/EditableBlockList.vue
+++ src/components/EditableBlockList.vue
@@ -24,7 +24,7 @@
             :target="config.openInNewWindow ? '_blank' : '_top'"
             class="list-item"
             style="position: relative"
-            :style="{'background-color': getRgbaString(item.backgroundColor.rgba), width: '100%', 'min-height': readonly ? 'initial' : '100px',  'height': readonly ? 'auto' : '100%'}"
+            :style="{'background-color': getRgbaString(item.backgroundColor.rgba), width: '100%', 'min-height': readonly ? 'initial' : '100px'}"
             @mouseover="item.isHoveringOver = true"
             @mouseleave="item.isHoveringOver = false"
           >
